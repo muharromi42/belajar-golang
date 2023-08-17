da24e2b83ab3bd114089aebea9e484739ed1a619
muharromi42 <muharromi42@gmail.com>
2023-08-17 23:29:48 +0700

    fallthrough

---
+++ chapter-A13-seleksiKondisi/kondisi6.go
@@ -0,0 +1,20 @@
+// A.13.6. Switch Dengan Gaya if - else
+package main
+
+import "fmt"
+
+func main()  {
+	var point = 6
+
+	switch {
+	case point == 8:
+		fmt.Println("perfect")
+	case (point < 8) && (point > 3):
+		fmt.Println("awesome")
+	default:
+		{
+			fmt.Println("not bad")
+			fmt.Println("you need to learn more	")
+		}
+	}
+}
